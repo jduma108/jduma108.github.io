a32ded960879c6fa4fbe785c1bf26e157064acbd
jduma108 <jduma108@gmail.com>
2020-08-24 12:57:14 -0500

    fixing grammer errors

--- src/data/porfolioData.js
+++ src/data/porfolioData.js
@@ -3,14 +3,14 @@ const PORTFOLIO_DATA = [
         "id": 0,
         "project": "Enklu",
         "role": "Web & Graphic Design",
-        "description1": "As a freelance designer, I assisted in creating wire-frame designs and marketing material for Enklu. Enklu is a start up company based in San Francisco that focuses on creating social experiences using augmented reality.",
+        "description1": "As a freelance designer, I created wire-frame designs and marketing material for Enklu. Enklu is a start up company based in San Francisco that focuses on creating social experiences using augmented reality.",
         "description2": "Using Adobe Illustrator, I designed wire-frames for Enklu's official website and used Sketch to design mock-ups for their mobile app. In addition to this, I created marketing material such as magazine advertisements, flyers, logos, icons and more using Adobe Illustrator, InDesign and Photoshop. During my time at Enklu, I worked with a multidisciplinary team that expanded and improved my knowledge of web and graphic design."
     },
     {
         "id": 1,
         "project": "Southwest",
         "role": "Illustration & Animation",
-        "description1": "Using Adobe Illustrator, I created technical illustrations for Southwest Airlines ETOPS training video. Southwest's Extended Operations (ETOPS) is a new program to assist Southwest employees in preparing for flights with destinations to Hawaii.",
+        "description1": "Using Adobe Illustrator, I created technical illustrations for Southwest Airlines Southwest's Extended Operations (ETOPS) training video. ETOPS is a new program to assist Southwest employees in preparing for flights with destinations to Hawaii.",
         "description2": "Besides the flight attendants and the Southwest Airline heart, I created the airplane cabin, airplane galley, oxygen tank, fire extinguisher, emergency locator transmitter (ELT) and life raft. In addition to designing illustrations, I assisted with animating the training video using Adobe After Effects."
     },
     {
@@ -18,27 +18,27 @@ const PORTFOLIO_DATA = [
         "project": "Tiger Center",
         "role": "Web Design",
         "description1": "As a web designer for RIT's Student I.T. Office (SITO), I created desktop and mobile wire-frames for Tiger Center, a web application built by students for students. This application allows users to enroll in classes, manage dining balances, locate places on campus and more.",
-        "description2": "These wire-frames helped establish a design direction for the Tiger Center redesign. The wire-frame documentation outlines the design for each webpage and the functionalities for both mobile and desktop."
+        "description2": "These wire-frames established a design direction for the Tiger Center redesign. The wire-frame documentation outlines the design for each webpage and the functionalities for both mobile and desktop."
     },
     {
         "id": 3,
         "project": "Priority Thinking",
         "role": "Web Design",
-        "description1": "As a freelance web designer for Priority Thinking LLC, I designed wire-frames for their web application called Admin Console. The purpose of this web application is to create surveys for small/medium companies to distribute to employees and provide insights based off of the results. I followed design features based off Metro UI, since the company chose to use this as their front-end framework.",
+        "description1": "As a freelance web designer for Priority Thinking LLC, I designed wire-frames for their web application called Admin Console. The purpose of this web application is to create surveys for small/medium companies to distribute to employees and provide insights based off of the results. I designed features based off Metro Ui, the company's front-end framework.",
         "description2": ""
     },
     {
         "id": 4,
         "project": "PHLOTE",
         "role": "Logo Design",
-        "description1": "Using Adobe Illustrator, I created these logos to be used as a design for a NASA mission patch. The mission that these logos will represent is called PHLOTE. The design was inspired by the concept of the PHLOTE mission and other drawings that were given to me by the employees who worked on PHLOTE. The logo designs have appeared on presentation posters, apparel and will be represented on the official report (link to the official report TBA).",
+        "description1": "Using Adobe Illustrator, I created these logos to be used as a design for a NASA mission patch, Phobos L1 Operational Tether Experiment (PHLOTE). The design was inspired by the concept of the PHLOTE mission and other drawings that were given to me by the employees who worked on PHLOTE. The logo designs have appeared on presentation posters, apparel and will be represented on the official report (link to the official report TBA).",
         "description2": ""
     },
     {
         "id": 5,
         "project": "Gig Reporter",
         "role": "Web Development",
-        "description1": "Using HTML, CSS, JavaScript, PHP and API keys from The New York Times and Ticketmaster website, I was able to design and create a website which allows users to search for concert tickets and articles about their favorite bands. This was the final project for my Webpage Production II course.",
+        "description1": "As a final project for Webpage Production II, I created Gigi Reporter, a website that allows users to search for concert tickets and articles about their favorite bands. This project was created using HTML, CSS, JavaScript, PHP and API keys from The New York Times and Ticketmaster website.",
         "description2": ""
     },
     {
@@ -52,7 +52,7 @@ const PORTFOLIO_DATA = [
         "id": 7,
         "project": "Brand Style Guide",
         "role": "Print & Pag Layout",
-        "description1": "As a final project for Print Production Workflow, I created and designed a fictional brand style guide that represetned myself. This project allowed me to experience the process of making a print piece from start to finish. I created a logo, color scheme and content to populate the guide. The guide was created using Adobe InDesign.",
+        "description1": "As a final project for Print Production Workflow, I created and designed a fictional brand style guide that represented myself. This project allowed me to experience the process of making a print piece from start to finish. I created a logo, color scheme and content to populate the guide. The guide was created using Adobe InDesign.",
         "description2": ""
     }
 ]
